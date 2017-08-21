7001de3dfce1990fd0b8c3dbbc06581f912639b9
马传林 <cma@maxleap.com>
2017-08-21 11:45:03 +0800

    fix docker file

--- bifrost-server/src/main/resources/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if  [ -z $HOST];then
-vertx run java-hk2:com.maxleap.bifrost.kotlin.BifrostServer --conf config.json $@
-else
-vertx run java-hk2:com.maxleap.bifrost.kotlin.BifrostServer --conf config.json -cluster -cluster-host $HOST $@
-fi
